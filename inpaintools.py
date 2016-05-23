c82b841c1592842e9bd4e96ca2e62d7ba73673c7
ZolatanCoccyx <bhecharles@carbon.guestcmla.ens-cachan.fr>
2016-05-23 10:32:01 +0200

    voisinage carré

---
+++ inpaintools.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 19 10:35:28 2016
+
+@author: Balthazar
+"""
+
+import pymaxflow
+import numpy as np
+
+## Alpha-expansion related functions
+
+def solve_binary_problem(indices, D0, D1, 
+                        e1, e2, V00, V01, V10, V11,
+                        _e1=None, _e2=None, _V00=None, _V01=None, _V10=None, _V11=None):
+    '''
+    solve the labeling problem     
+        min_x    \sum_i  D_i(x_i) + \sum_{ij}  V_ij(x_i,x_j)     
+        with x_i \in {0,1}
+    '''
+    sz = D0.size
+    sh = D0.shape
+    e  = pymaxflow.PyEnergy(sz, sz*4)
+
+    # variables
+    first_node_idx = e.add_variable(sz)
+    
+    # add unary terms                     
+    e.add_term1_vectorized(indices.ravel(), D0.ravel(), D1.ravel())
+    
+    # add binary terms                     
+    e.add_term2_vectorized(e1.ravel() , e2.ravel() , 
+                           V00.ravel(), V01.ravel(), 
+                           V10.ravel(), V11.ravel())
+    # add more binary terms                     
+    if type(_V11) != type(None):
+        e.add_term2_vectorized(_e1.ravel() , _e2.ravel() , 
+                               _V00.ravel(), _V01.ravel(), 
+                               _V10.ravel(), _V11.ravel())
+    Emin = e.minimize()       
+    out  = e.get_var_vectorized()
+    return Emin, out.reshape(sh)
+ 
+## Image displacements : circular shifts, warps, calculation of displacement maps
+   
+def shift_image(im, dx, dy):
+    ''' circular shift of dx, dy'''
+    cim=np.copy(im)
+    return np.roll(np.roll(cim,dy,axis=0),dx,axis=1)
+    
+def warp(im,mx,my):
+    '''''' 
+    sh = im.shape
+    
+    px, py = np.meshgrid(range(sh[1]), range(sh[0]))
+    px += mx 
+    py += my
+    
+    #truncated 
+    px = np.mod(px,sh[1])
+    py = np.mod(py,sh[0])#    
+    Out = im[py[:],px[:],] 
+    return Out
+    
+def compute_displacement_map(m, shifts, mask=None):
+    M = np.copy(mask)
+    nlabels = len(shifts)
+
+    # no displacement map
+    mx, my = m * 0, m * 0
+    
+    for t in range(nlabels):
+        mx += shifts[t][0] * (m==t)
+        my += shifts[t][1] * (m==t)
+        
+    #apply mask
+    if type(mask)!=type(None):
+        mx = mx * (M>0)
+        my = my * (M>0)
+    return mx, my
+
+## Distance
+
+def diff_image(a, b, p = 2):
+    if len(a.shape)==3:
+        return np.sum(np.abs(a - b)**p, axis=2) ** (1 / p)
+    else:
+        return np.abs(a - b)
+
+def difflabel(mx, my, mx1, my1):
+    eps = 20
+    k2 = 5
+    K2 = k2 * np.ones(mx.shape)
+    N = np.sqrt((mx - mx1) ** 2 + (my - my1) ** 2)
+    return (eps * mint(N, K2)).astype(np.float32)
+
+## Utilitaries
+        
+def intensity(im):
+    return(1/3*np.sum(im,axis=2))
+    
+def mint(A,B):
+    return 1 * (A * (A <= B) + B * (B < A))
+
+def frontiere(mask, neighborhood):
+   M = 1 - mask
+   K = M * 0
+   n = len(neighborhood)
+   for i, j in zip(neighborhood[:,0], neighborhood[:,1]):
+       K += shift_image(M, i, j)  
+   F = K * 0
+   for i in range(1, n):
+       F = F + (K == i)
+       F = F.astype(np.float32)
+   return F * mask
+   
+def penaltydata(mx,my,mask):
+    sh = mask.shape
+    px, py = np.meshgrid(range(sh[1]), range(sh[0]))
+    px += mx 
+    py += my
+    C=1500000
+    K1=C*(px>(sh[1]-1))
+    K2=C*(py>(sh[0]-1))
+    K3=C*(px<0)
+    K4=C*(py<0)
+    px = np.mod(px,sh[1])
+    py = np.mod(py,sh[0])
+    I=C*mask[py,px]
+    return (I+K1+K2+K3+K4)*mask
+
+def square_neighborhood(n):
+    dx, dy = np.meshgrid(np.arange(-n // 2 + 1, n // 2 + 1), np.arange(-n//2 + 1, n // 2 + 1 ))
+    dx, dy = dx.ravel(), dy.ravel()
+    return np.array([dx, dy]).T
---
+++ shift map bien (1).py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 24 14:08:41 2016
+
+@author: Rémi
+"""
+
+import numpy as np
+from scipy.misc import imread
+from matplotlib import pyplot as plt
+#from skimage import io
+from time import time
+import inpaintools
+  
+w=lambda x : np.exp(-0.25*(x**2))    
+    
+def Data(im, mx, my, mask, F, neighborhood):
+    #F=frontiere(mask,neighborhood)
+    M = np.copy(mask)
+    I = im * (np.dstack((M==0, M==0, M==0)))
+    e = []
+    E = []
+    D = []
+    S = np.zeros(mask.shape)
+    Data = np.zeros(mask.shape)
+    for i, j in zip(neighborhood[:,0], neighborhood[:,1]):
+        u = shift_image(I, i, j)
+        v = warp(u, mx, my)
+        e.append(u)
+        E.append(v)
+        x = (i ** 2 + j ** 2) ** 0.5
+        D.append(w(x) * ((diff_image(u,v) ** 2) * ((u[:,:,0]>0) * (v[:,:,0]>0))))
+        S = S + w(x) * ((v[:,:,0]>0) * (u[:,:,0]>0))
+    S2 = S + ((S * F)==0)
+    for Dataterm in D:
+        Data = Data + (1 / S2) * Dataterm
+    P = penaltydata(mx, my, mask)
+    return ((Data + 5000000 * ((S * F)==0)) * (F > 0)) + P
+    
+def Datat(im, mask, shifts, neighborhood = np.array([[0,1],[0,-1],[0,2],[0,-2],[1,0],[-1,0],[2,0],[-2,0]])):
+    F = frontiere(mask,neighborhood)
+    n, k, v = im.shape
+    l = shifts.shape[0]
+    D = np.zeros((n,k,l))
+    for m in range(l):
+        mx = shifts[m,0]
+        my = shifts[m,1]
+        Mx = mx * np.ones((n,k)) * mask
+        My = my * np.ones((n,k)) * mask
+        Dm = Data(im, Mx, My, mask, F, neighborhood)
+        D[:, :, m] = Dm
+    return D     
+
+def shiftmaps(im, mask, shifts,D, rounds = 1):
+    neighborhood = np.array([[0,1],[0,-1],[1,0],[-1,0]])
+    F = frontiere(mask, neighborhood)
+    M = np.copy(mask * (F==0))
+    F2 = frontiere(mask, np.array[[1,0],[0,1]])
+    M = M + F2
+    nlabels = len(shifts)
+    sz = np.prod(mask.shape[0:2])
+    sh = mask.shape[0:2]
+    
+    indices = np.arange(sz).reshape(sh).astype(np.int32)
+
+    # generate a random initial labeling
+    
+    labelmap = np.random.randint(nlabels,size=sz).reshape(sh) * (mask>0) *0
+    mx, my = compute_displacement_map(labelmap, shifts, mask)
+    #D0=Data(im,mx,my,mask,F).astype(np.float32)
+    E = 2000000 + np.sum(D[:, :, 0])
+     
+        
+    # alpha expansion loop
+    for t in range(rounds*len(shifts)):
+        currlab = np.mod(t,len(shifts))
+        ai,aj = shifts[currlab]
+        # compute warp for the current map
+        mx, my = compute_displacement_map(labelmap, shifts, mask)
+                
+        # compute warped image for the current map 
+        outM = warp(im, mx, my) 
+        # compute warped image for the candidate shift 
+        outAlpha = warp(im, (mx*0+ai)*mask, (my*0+aj)*mask)    # can be precomputed
+
+          
+        # construct the baseline regularity term 4-connected           
+        for i,j in ((1,0),(0,1)):
+            tmpx = shift_image(mx,-i,-j) 
+            tmpy = shift_image(my,-i,-j)
+            outQ = warp(im, tmpx, tmpy)
+            
+            #p+m(q)   
+            p1 = indices                      # current pixel
+            p2 = shift_image(indices,-i,-j)   # always the same idx
+            
+            if(i==1):
+                E00 = (diff_image(outM, outQ) + difflabel(mx, my, tmpx, tmpy)) * M
+                E01 = (diff_image(outM, outAlpha) + difflabel(mx, my, mx * 0 + ai, my * 0 + aj)) * M
+                E10 = (diff_image(outAlpha, outQ) + difflabel(tmpx, tmpy, mx * 0 + ai, my * 0 + aj)) * M
+                E11 = 0 * E00   # 0
+                e1, e2 = p1, p2
+               
+                    
+            else:
+                _E00 = (diff_image(outM, outQ) + difflabel(mx, my, tmpx, tmpy))*M
+                _E01 = (diff_image(outM, outAlpha) + difflabel(mx, my, mx * 0 + ai, my * 0 + aj)) * M
+                _E10 = (diff_image(outAlpha, outQ) + difflabel(tmpx, tmpy, mx * 0 + ai, my * 0 + aj)) * M
+                _E11 = 0 * E00                  
+                _e1,_e2 = p1,p2
+               
+        
+        # data terms
+        # TODO: not too sure about these boundary conditions
+     
+#        D0=Data(im,mx,my,mask,F).astype(np.float32)
+#        D1=Data(im,(mx*0+ai)*mask, (my*0+aj)*mask,mask,F).astype(np.float32)
+
+        ix, iy = np.meshgrid(np.arange(sh[1]), np.arange(sh[0]))
+#        
+        D0 = D[iy, ix, labelmap].astype(np.float32)
+        D1 = D[:, :, currlab].astype(np.float32)
+        
+        # trim the last row/column of the edge matrices
+        energy, blab = solve_binary_problem(indices, D0, D1, 
+                        e1[:,:-1], e2[:,:-1], E00[:,:-1], E01[:,:-1], E10[:,:-1], E11[:,:-1],
+                        _e1[:-1,:], _e2[:-1,:], _E00[:-1,:], _E01[:-1,:], _E10[:-1,:], _E11[:-1,:])
+        
+        # update solution  
+        if energy<E:
+            E=energy
+            print "it:%02d \t l:%d \t (%d,%d) \t changed:%d \t e:%f"%(t, currlab, ai, aj, np.sum(blab[:]), energy)
+            labelmap = labelmap * (1 - blab) + currlab * blab
+            
+    mx, my = compute_displacement_map(labelmap, shifts, mask)
+    outM = warp(im, mx, my)
+    
+    return outM, labelmap    
+    
+
+maxshift = 200
+numshifts = 250
+shifts = np.random.randint(maxshift,size=numshifts*2).reshape((numshifts,2)) - maxshift/2 
+
+im = imread('elephant2_300x225_rgb.jpg').squeeze().astype(np.float32)
+mask = imread('elephant2_300x225_msk.jpg').squeeze().astype(np.float32)
+
+#im = imread('foule.jpg').squeeze().astype(np.float32)
+#
+#mask = imread('foulem.jpg').squeeze().astype(np.float32)
+#mask=mask[:,:,0]
+#im=np.dstack((im,im,im))
+
+mask = mask > 10
+  
+T0 = time()
+
+D=Datat(im,mask,shifts)  
+
+out, labelmap = shiftmaps(im,mask,shifts,D,rounds=2)
+T1=time.time() 
+print(T1-T0)      
